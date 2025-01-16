f8be683da95ffb65c798c7f68c8547f3564b6fd7
Akshay Meher <akshay.meher@dynamisch.co>
2025-01-16 21:55:25 +0530

    change migration ensures that you only add or remove the column if necessary

--- src/migrations/20241227053345-add-appointment-time-to-appointments.js
+++ src/migrations/20241227053345-add-appointment-time-to-appointments.js
@@ -9,10 +9,22 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-    await queryInterface.addColumn('Appointments', 'appointment_time', {
-      type: Sequelize.TIME,
-      allowNull: false,
-    });
+    // await queryInterface.addColumn('Appointments', 'appointment_time', {
+    //   type: Sequelize.TIME,
+    //   allowNull: false,
+    // });
+
+
+    // Check if the 'appointment_time' column already exists in the 'Appointments' table
+    const tableInfo = await queryInterface.describeTable('Appointments');
+
+    if (!tableInfo.hasOwnProperty('appointment_time')) {
+      // Add the column if it doesn't exist
+      await queryInterface.addColumn('Appointments', 'appointment_time', {
+        type: Sequelize.TIME,
+        allowNull: false,
+      });
+    }
   },
 
   async down(queryInterface, Sequelize) {
@@ -22,6 +34,13 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
-    await queryInterface.removeColumn('Appointments', 'appointment_time');
+    // await queryInterface.removeColumn('Appointments', 'appointment_time');
+    // Check if the 'appointment_time' column exists in the 'Appointments' table
+    const tableInfo = await queryInterface.describeTable('Appointments');
+
+    if (tableInfo.hasOwnProperty('appointment_time')) {
+      // Remove the column if it exists
+      await queryInterface.removeColumn('Appointments', 'appointment_time');
+    }
   }
 };
